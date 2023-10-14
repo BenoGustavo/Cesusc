161e2b9318cc5d1d373553e1dcfc77ab353105dc
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-10-14 18:28:36 -0300

    main update

--- linguagemDeProgramacao/Java/DatabaseConnectionInJava/src/Main.java
+++ linguagemDeProgramacao/Java/DatabaseConnectionInJava/src/Main.java
@@ -1,5 +1,20 @@
+import java.sql.SQLException;
+
+import database.DataBase;
+import utils.TerminalMenu;
+
+// dataBase.createNewUser("John", 20, "John@gmail.com", "1234");
+// dataBase.createNewProduct("MacBook Pro", 1000, "A laptop from Apple");
+// dataBase.createNewOrder(1, 1);
+
 class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws SQLException {
+        int userChoice;
+
+        DataBase dataBase = new DataBase();
+
+        do {
+            userChoice = TerminalMenu.mainMenu();
+        } while (!TerminalMenu.MAIN_MENU_OPTIONS.contains(userChoice));
     }
 }
