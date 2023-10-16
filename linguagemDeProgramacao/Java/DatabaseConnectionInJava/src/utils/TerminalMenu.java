485fc7b600241c35ad80ab6b1a96ed422472fb90
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-10-16 17:53:18 -0300

    utils codes

--- linguagemDeProgramacao/Java/DatabaseConnectionInJava/src/utils/TerminalMenu.java
+++ linguagemDeProgramacao/Java/DatabaseConnectionInJava/src/utils/TerminalMenu.java
@@ -14,6 +14,8 @@ public class TerminalMenu {
     public final static ArrayList<Integer> ORDERS_MENU_OPTIONS = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
 
     public static String mainMenu() throws IOException {
+        String choice;
+
         System.out.println("-=-=-Order-Manager-=-=-\n");
         System.out.println("1 - Login");
         System.out.println("2 - Manage users");
@@ -21,28 +23,34 @@ public class TerminalMenu {
         System.out.println("4 - Manage orders");
         System.out.println("5 - Exit");
 
-        // Read and return user input
-        try (BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in))) {
-            return userInput.readLine();
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+        choice = userInput.readLine();
+
+        return choice;
+    }
+
+    public static void whoIsLoggedIn(ArrayList<String> userInfo) {
+        TerminalMenu.clearTerminal();
+        if (userInfo.isEmpty()) {
+            System.out.println("No user is logged in");
+            return;
         }
+        System.out.println("Logged in as: " + userInfo.get(1));
     }
 
     public static ArrayList<String> loginMenu() throws IOException {
         String username;
         String password;
 
+        clearTerminal();
         System.out.println("-=-=-Login-=-=-\n");
         System.out.println("Enter user: ");
 
-        try (BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in))) {
-            username = userInput.readLine();
-        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        username = reader.readLine();
 
         System.out.println("Enter password: ");
-
-        try (BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in))) {
-            password = userInput.readLine();
-        }
+        password = reader.readLine();
 
         ArrayList<String> credentials = new ArrayList<String>();
         credentials.add(username);
@@ -52,6 +60,7 @@ public class TerminalMenu {
     }
 
     public static String usersMenu() throws IOException {
+        clearTerminal();
         System.out.println("-=-=-Users-=-=-\n");
         System.out.println("1 - Create new user");
         System.out.println("2 - Update user");
@@ -63,6 +72,51 @@ public class TerminalMenu {
         try (BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in))) {
             return userInput.readLine();
         }
+    }
+
+    public static ArrayList<String> createNewUserMenu() throws IOException {
+        String name;
+        int age;
+        String email;
+        String password;
+
+        ArrayList<String> userData = new ArrayList<String>();
+
+        clearTerminal();
+        System.out.println("-=-=-Create new user-=-=-\n");
+        System.out.println("Enter name: ");
+
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+        name = userInput.readLine();
+
+        clearTerminal();
+        System.out.println("Enter age: ");
+        age = Integer.parseInt(userInput.readLine());
+
+        clearTerminal();
+        System.out.println("Enter email: ");
+        email = userInput.readLine();
+
+        clearTerminal();
+        System.out.println("Enter password: ");
+        password = userInput.readLine();
+
+        userData.add(name);
+        userData.add(String.valueOf(age));
+        userData.add(email);
+        userData.add(password);
+
+        return userData;
+    }
+
+    public static String deleteUserMenu() throws IOException {
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
+        clearTerminal();
+
+        System.out.println("-=-=-Delete user-=-=-\n");
+        System.out.println("Enter user id: ");
+        return userInput.readLine();
     }
 
     public static String productsMenu() throws IOException {
