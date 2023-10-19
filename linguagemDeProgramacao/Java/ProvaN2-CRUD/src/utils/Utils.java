046ca28af24446006ae2cfebf4fe8af43006bb66
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-10-19 10:27:09 -0300

    add src dir

---
+++ linguagemDeProgramacao/Java/ProvaN2-CRUD/src/utils/Utils.java
@@ -0,0 +1,17 @@
+package utils;
+
+import java.util.ArrayList;
+
+import database.ProductsModel;
+
+public class Utils {
+    public static ProductsModel findProduct(ArrayList<ProductsModel> listOfProducts, int id) {
+
+        for (ProductsModel Product : listOfProducts) {
+            if (Product.getId() == id) {
+                return Product;
+            }
+        }
+        return null;
+    }
+}
