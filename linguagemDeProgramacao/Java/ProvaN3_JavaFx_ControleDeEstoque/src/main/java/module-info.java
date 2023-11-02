349b2fb006e77c50bb65f0a8511de0344591628b
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-11-02 18:12:35 -0300

    added java.sql to requirement

--- linguagemDeProgramacao/Java/ProvaN3_JavaFx_ControleDeEstoque/src/main/java/module-info.java
+++ linguagemDeProgramacao/Java/ProvaN3_JavaFx_ControleDeEstoque/src/main/java/module-info.java
@@ -2,6 +2,9 @@ module com.controleestoque {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires java.sql;
+
     opens com.controleestoque to javafx.fxml;
+
     exports com.controleestoque;
 }
