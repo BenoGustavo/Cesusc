b79116212b3de85fc85cb1ebd465be5b1ad890ba
BenoGustavo <gustavoleandrogorges@gmail.com>
2024-04-01 21:30:10 -0300

    init js

---
+++ linguagemDeProgramacao2/aula2/exercises/js/exercise-5/script.js
@@ -0,0 +1,9 @@
+// <= 2.112
+// >= 2112.1 && <= 2826.65
+// >= 2826.66 && <= 3751.05
+// >= 3751.06 && < 4664.68
+// >= 4664.68
+
+hourValue = document.getElementById('hour').value;
+workedHours = document.getElementById('workedHours').value;
+
