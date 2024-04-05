ca1a129a9fc91cc7e658ca8e76884fac592102f9
BenoGustavo <gustavoleandrogorges@gmail.com>
2024-04-05 16:48:01 -0300

    finished prova n1

--- linguagemDeProgramacao2/provaN1/scripts/employee/script.js
+++ linguagemDeProgramacao2/provaN1/scripts/employee/script.js
@@ -1,29 +1,19 @@
-createButton = document.getElementById("criar_funcionario")
+window.onload = function () {
+    document.addEventListener('click', function (event) {
+        if (event.target.classList.contains('editar_funcionario')) {
+            editEmployee();
+        } else if (event.target.classList.contains('excluir_funcionario')) {
+            deleteEmployee();
+        }
+    });
+}
 
-createButton.addEventListener('click', createEmployee);
+function editEmployee() {
+    alert("Funcionário editado com sucesso!");
+    return;
+}
 
-function createEmployee(){
-    const nameField = document.getElementById("employee_name").value
-    const RG = document.getElementById("RG").value
-    const CPF = document.getElementById("CPF").value
-    const DATA = document.getElementById("data_nascimento").value
-    const Telefone = document.getElementById("telefone").value
-    const Email = document.getElementById("email").value
-    const endereco = document.getElementById("endereco").value
-
-    let listOfInputs = [nameField,RG,CPF,DATA,Telefone,Email,endereco]
-
-    let table = document.getElementsByClassName("table__class")
-
-    let row = document.createElement("tr");
-
-    for (let index = 0; index < listOfInputs.length; index++){
-        let cell = document.createElement("td");
-
-        cell.value = listOfInputs[index];
-
-        row.appendChild(cell);
-    }
-    console.log(table)
-    table.appendChild(row)
+function deleteEmployee() {
+    alert("Funcionário deletado com sucesso!");
+    return;
 }
