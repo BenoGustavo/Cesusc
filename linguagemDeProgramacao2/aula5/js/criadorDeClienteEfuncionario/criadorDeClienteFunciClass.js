e2c24358f6e44ea0f75f1dd71bb1d94acf1f6ebc
BenoGustavo <gustavoleandrogorges@gmail.com>
2024-05-05 15:54:05 -0300

    finished creating func and cli class

---
+++ linguagemDeProgramacao2/aula5/js/criadorDeClienteEfuncionario/criadorDeClienteFunciClass.js
@@ -0,0 +1,37 @@
+class Pessoa {
+    constructor(nome, cpf, dataNascimento) {
+        this.nome = nome
+        this.cpf = cpf
+        this.dataNascimento = dataNascimento
+    }
+}
+
+class Cliente extends Pessoa {
+    constructor(nome, cpf, dataNascimento, email, telefone) {
+        super(nome, cpf, dataNascimento)
+        this.email = email
+        this.telefone = telefone
+    }
+
+    updateCliente(novoNome = this.novoNome, novoCpf = this.cpf, novaDataNascimento = this.dataNascimento, novoEmail = this.email, novoTelefone = this.telefone) {
+        this.nome = novoNome
+        this.cpf = novoCpf
+        this.dataNascimento = novaDataNascimento
+        this.email = novoEmail
+        this.telefone = novoTelefone
+    }
+}
+
+class Funcionario extends Pessoa {
+    constructor(nome, cpf, dataNascimento, salario) {
+        super(nome, cpf, dataNascimento)
+        this.salario = salario
+    }
+
+    updateFuncionario(novoNome = this.novoNome, novoCpf = this.cpf, novaDataNascimento = this.dataNascimento, novoSalario = this.salario) {
+        this.nome = novoNome
+        this.cpf = novoCpf
+        this.dataNascimento = novaDataNascimento
+        this.salario = novoSalario
+    }
+}
