fb2b0a22fdb703edb6c20d58aa6e708203b96a17
BenoGustavo <gustavoleandrogorges@gmail.com>
2024-05-06 15:12:36 -0300

    chore: Refactor class exports and add controladoras for Funcionario and Cliente

--- linguagemDeProgramacao2/aula5/js/criadorDeClienteEfuncionario/criadorDeClienteFunciClass.js
+++ linguagemDeProgramacao2/aula5/js/criadorDeClienteEfuncionario/criadorDeClienteFunciClass.js
@@ -6,7 +6,7 @@ class Pessoa {
     }
 }
 
-export class Cliente extends Pessoa {
+class Cliente extends Pessoa {
     constructor(nome, cpf, dataNascimento, email, telefone) {
         super(nome, cpf, dataNascimento)
         this.email = email
@@ -22,7 +22,7 @@ export class Cliente extends Pessoa {
     }
 }
 
-export class Funcionario extends Pessoa {
+class Funcionario extends Pessoa {
     constructor(nome, cpf, dataNascimento, salario) {
         super(nome, cpf, dataNascimento)
         this.salario = salario
@@ -33,5 +33,69 @@ export class Funcionario extends Pessoa {
         this.cpf = novoCpf
         this.dataNascimento = novaDataNascimento
         this.salario = novoSalario
+    }
+}
+
+export class FuncionarioControladora {
+    constructor() {
+        this.funcionarios = []
+    }
+
+    create(data) {
+        const funcionario = new Funcionario(data[0], data[1], data[2], data[3])
+        this.add(funcionario)
+    }
+
+    add(funcionario) {
+        this.funcionarios.push(funcionario)
+    }
+
+    delete(cpf) {
+        this.funcionarios = this.funcionarios.filter(funcionario => funcionario.cpf !== cpf)
+    }
+
+    update(cpf, novoNome, novoCpf, novaDataNascimento, novoSalario) {
+        const funcionario = this.funcionarios.find(funcionario => funcionario.cpf === cpf)
+        funcionario.updateFuncionario(novoNome, novoCpf, novaDataNascimento, novoSalario)
+    }
+
+    get(cpf) {
+        return this.funcionarios.find(funcionario => funcionario.cpf === cpf)
+    }
+
+    getAll() {
+        return this.funcionarios
+    }
+}
+
+export class ClienteControladora {
+    constructor() {
+        this.clientes = []
+    }
+
+    create(data) {
+        const cliente = new Cliente(data[0], data[1], data[2], data[3], data[4])
+        this.add(cliente)
+    }
+
+    add(cliente) {
+        this.clientes.push(cliente)
+    }
+
+    delete(cpf) {
+        this.clientes = this.clientes.filter(cliente => cliente.cpf !== cpf)
+    }
+
+    update(cpf, novoNome, novoCpf, novaDataNascimento, novoEmail, novoTelefone) {
+        const cliente = this.clientes.find(cliente => cliente.cpf === cpf)
+        cliente.updateCliente(novoNome, novoCpf, novaDataNascimento, novoEmail, novoTelefone)
+    }
+
+    get(cpf) {
+        return this.clientes.find(cliente => cliente.cpf === cpf)
+    }
+
+    getAll() {
+        return this.clientes
     }
 }
